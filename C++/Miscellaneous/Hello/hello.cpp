e22859e8b67e4fcce7ccb63b6449074a5be14c25
Antonio Alvarado <tnotstar@gmail.com>
2013-10-17 13:55:34 +0200

    created a minimalistic Makefile for C++
    * added artificial header and program unit files

---
+++ C++/Miscellaneous/Hello/hello.cpp
@@ -0,0 +1,14 @@
+//
+// hello.cpp: a simple "Hello, world!" program
+//
+
+#include <iostream>
+#include "hello.h"
+
+using namespace std;
+
+int
+main () {
+    cout << message << endl;
+    return 0;
+}
