b440def11996272b5fe73b8882d0e7a8ec676047
Antonio Alvarado <tnotstar@gmail.com>
2013-11-15 18:21:25 +0100

    created a C::B project for HEX
    * added a C::B project file.
    * added a graph implementation based on adjacency lists.
    * minor changes on legacy files.

--- C++/Learn/ira_hw04_hex.cpp
+++ C++/Learn/ira_hw04_hex.cpp
@@ -2,7 +2,7 @@
 // ira_hw03_mst.cpp: Homework #4 from "C++ for C programmers".
 //
 
-#include "dense_graph.hpp"
+#include "sparse_graph.hpp"
 
 #include <iostream>
 
@@ -16,7 +16,7 @@ const size_t DEFAULT_GRAPH_SIZE = 1000000;
 int
 main () {
 
-    dense_graph<double> g(DEFAULT_GRAPH_SIZE);
+    sparse_graph<int, double, double> g(DEFAULT_GRAPH_SIZE);
 
     g.add_edge(0, 1, 1.2);
     g.add_edge(0, 3, 1.2);
@@ -24,6 +24,7 @@ main () {
     g.add_edge(2, 3, 1.2);
     g.add_edge(3, 4, 1.2);
 
+    cout << g << endl;
+
     return 0;
 }
-
