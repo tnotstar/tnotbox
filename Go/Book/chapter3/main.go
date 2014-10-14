026f79ad7a881c39d03af041f1ca89eb71eeb99d
Antonio Alvarado H. <tnotstar@gmail.com>
2014-10-14 17:53:46 +0200

    added sample code from `golang-book`
    * created a new folder to store book samples
    * created first two samples

---
+++ Go/Book/chapter3/main.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+// this is a line comment
+
+func main() {
+	fmt.Println("1 + 1 =", 1 + 1)
+	fmt.Println("4 % 2 =", 4 % 2)
+	
+	fmt.Println("1.0 + 1.0 =", 1.0 + 1.0)
+	fmt.Println("1.0 - 1.0 =", 1.0 - 1.0)
+	fmt.Println("1.0 * 2.0 =", 1.0 * 2.0)
+	fmt.Println("1.0 / 2.0 =", 1.0 / 2.0)
+
+	fmt.Println("Hello, world!")
+	fmt.Println("Hello, world!"[0])
+	fmt.Println(len("Hello, world!"))
+	fmt.Println("Hello, " + "world!")
+	fmt.Println(`
+  Hello, world
+`)
+
+	fmt.Println("true && true =", true && true)
+	fmt.Println("true && false =", true && false)
+	fmt.Println("true || true =", true || true)
+	fmt.Println("true || false =", true || false)
+	fmt.Println("!true =", !true)
+	fmt.Println("!false =", !false)
+}
+/*
+Estas tendencias tienen su origen en fuerzas muy fundamentales
+y difíciles de contrarrestar: la productividad cada vez mayor de las
+máquinas inteligentes, la globalización económica y la escisión de
+las economías contemporáneas en sectores muy estancados y sec-
+tores muy dinámicos.
+*/
+
+/*
+Este desequilibrio en el crecimiento tecnológico va a tener algunas
+consecuencias sorprendentes. Por ejemplo, cada vez más se clasificará
+a los trabajadores en dos categorías. Las preguntas decisivas serán: ¿Se
+le da bien trabajar con máquinas inteligentes o no? ¿Complementan
+sus conocimientos los conocimientos del ordenador, o éste funciona
+mejor sin usted? Peor aún, ¿está usted compitiendo contra el ordenador?
+¿Están ayudando los ordenadores a personas de China e India a com-
+petir contra usted?
+*/
