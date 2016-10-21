1b2b88d23ebcc6766c888a35eb35ec0f5c1eede6
Antonio Alvarado H <tnotstar@gmail.com>
2016-10-21 18:47:29 +0200

    variables examples from _A Tour of Go_
      * added `var` statement example
      * added variable initializers example

---
+++ Go/Learn/11_variables-with-initializers.go
@@ -0,0 +1,14 @@
+/* 11_variables-with-initializers.go */
+
+package main
+
+import "fmt"
+
+var i, j int = 1, 2
+
+func main() {
+    var c, python, java = true, false, "no!"
+    fmt.Println(i, j, c, python, java)
+}
+
+/* EOF */
