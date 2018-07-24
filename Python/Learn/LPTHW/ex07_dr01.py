b434a0822b136be94fd97e8d9f441c3f17178484
Antonio Alvarado H <tnotstar@gmail.com>
2018-07-24 07:53:23 +0200

    add exercise #07 the LPTHW
    - created exercise file and its drill

---
+++ Python/Learn/LPTHW/ex07_dr01.py
@@ -0,0 +1,27 @@
+# print a text value
+print("Mary had a little lamb.")
+# print a formatted string value
+print("Its fleece was white as {}.".format('snow'))
+# again, print a text value
+print("And everywhere that Mary went.")
+# print a line of dots of length 10
+print("." * 10)
+
+# assign letters from "CheeseBurger" to some variables
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# print the "Cheese Burger" string value by string concatenation
+# watch end = ' ' at the end. try removing it to see what happens
+print(end1 + end2 + end3 + end4 + end5 + end6, end=" ")
+print(end7 + end8 + end9 + end10 + end11 + end12)
