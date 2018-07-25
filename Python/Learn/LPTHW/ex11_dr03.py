181520d47305490607f745db6d4b6e3ec66f14f2
Antonio Alvarado H <tnotstar@gmail.com>
2018-07-25 10:17:44 +0200

    add exercise #11 to the LPTHW
    - created exercise file and its drills

---
+++ Python/Learn/LPTHW/ex11_dr03.py
@@ -0,0 +1,5 @@
+name = input("What's your name? ")
+salary = input("What's your current salary? ")
+company = input("Where do you work? ")
+
+print(f"Hello, I'm {name}, currently I work at {company} earning {salary}.")
