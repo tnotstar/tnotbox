d0dd48c4232d01e75ea9954e8099adc703941b53
Antonio Alvarado H <tnotstar@gmail.com>
2018-07-26 23:34:22 +0200

    add exercise #13 to the LPTHW
    - created exercise file and its drills

---
+++ Python/Learn/LPTHW/ex13_dr02b.py
@@ -0,0 +1,9 @@
+from sys import argv
+# read the WYSS section for how to run this
+script, first, second, third, fourth = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Yout third variable is:", third)
+print("Yout fourth variable is:", fourth)
