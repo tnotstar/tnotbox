bf0d31aa47945a7c87d3bf7e2e12120c2fdc9c4b
Antonio Alvarado H <tnotstar@gmail.com>
2019-02-12 23:41:31 +0100

    added a new `LOVE` sample about variables
    - created game folder `03_vars`
    - added files `conf.lua` and `main.lua`

---
+++ LOVE/Learn/HowToLOVE/03_vars/main.lua
@@ -0,0 +1,21 @@
+A = 23
+B = 55
+C = A + B
+A = 1
+print(C)
+
+var_123 = 123
+
+sheap = 10
+SHEAP = 20
+
+print(sheap)
+print(SHEAP)
+
+print("Hello, world!")
+
+animal = "sheep"
+name = "antonio"
+
+print(animal, name)
+print(name .. " is a " .. animal)
