e506449d845610fd4fb10d370f89ae4a9b5d3a80
Antonio Alvarado Hernández <tnotstar@gmail.com>
2019-09-24 15:33:49 +0200

    reused legacy `hello-world` for Uday's tutorial
    - created a folder for Uday's tutorial
    - renamed legacy `Hello` to tutorial's notation
    - changed code with comments and return types

---
+++ Dart/Learn/ThatIsUday/a_hello-world/1_hello.dart
@@ -0,0 +1,9 @@
+// Stolen from: https://itnext.io/e17c35a60c54
+
+/**
+ * `main` function is the entry point of execution
+ * when a .dart file is executed using `dart` command
+ */
+void main() {
+    print( "Hello World" ); // print is built-in function to print an object to the STDOUT
+}
