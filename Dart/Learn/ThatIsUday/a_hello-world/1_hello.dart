d97a880a47cab7b30753bfa5d4af4618413cc4fc
Antonio Alvarado Hernández <tnotstar@gmail.com>
2019-09-24 15:44:22 +0200

    added an example of main's arguments in Dart
    - created a new example with command line arguments
    - fixed minor issues of `1_hello.dart`

--- Dart/Learn/ThatIsUday/a_hello-world/1_hello.dart
+++ Dart/Learn/ThatIsUday/a_hello-world/1_hello.dart
@@ -1,9 +1,9 @@
 // Stolen from: https://itnext.io/e17c35a60c54
 
 /**
- * `main` function is the entry point of execution
- * when a .dart file is executed using `dart` command
+ * `main` function is the entry point of execution when a .dart file
+ * is executed using `dart` command (the dart vm)
  */
 void main() {
-    print( "Hello World" ); // print is built-in function to print an object to the STDOUT
+    print("Hello, world!"); // print a string to the standard output
 }
