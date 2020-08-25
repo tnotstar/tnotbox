58d15e334504decff3c4049e3413da85d91d5d3a
Antonio Alvarado Hernández <tnotstar@gmail.com>
2020-08-25 22:12:41 +0200

    added support for `cargo build` in `01_HelloWorld`
    - moved source files to `src` directory
    - added generated `Cargo.toml` project file

---
+++ Rust/Learn/ByExample/01_HelloWorld/src/hello.rs
@@ -0,0 +1,10 @@
+//
+// This is a comment, and is ignored by the compiler
+//
+
+fn main() {
+    // Statements here are executed when the compiled binary is called
+
+    // Following statement prints text to the console
+    println!("Hello, world!");
+}
