49a05cbef6bd430d70ffedba4ea9d2d4ec91e57a
Antonio Alvarado Hernández <tnotstar@gmail.com>
2020-09-09 19:12:23 +0200

    added a basic example of `hyper` use
    - created a binary project with the first sample
      use of `hyper`

---
+++ Rust/3rdParty/hyper/client-basic/src/main.rs
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2020 Antonio Alvarado Hernández
+ */
+
+extern crate hyper;
+
+use hyper::Client;
+use hyper::body::HttpBody as _;
+use tokio::io::{stdout, AsyncWriteExt as _};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+
+    let client = Client::new();
+    let uri = "http://httpbin.org/ip".parse()?;
+    let mut resp = client.get(uri).await?;
+    println!("Response: {}", resp.status());
+
+    while let Some(chunk) = resp.body_mut().data().await {
+        stdout().write_all(&chunk?).await?;
+    }
+
+    Ok(())
+}
