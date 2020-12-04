f826ad4524898fad5e6a182876407264288755cf
Antonio Alvarado Hernández <tnotstar@gmail.com>
2020-12-04 19:01:22 +0100

    moved legacy node's learning examples
    - updated `environ.js` to show `HOME` environ variable

--- NodeJS/Learn/Legacy/environ.js
+++ NodeJS/Learn/Legacy/environ.js
@@ -7,7 +7,7 @@ console.log("Testing environment objects...");
 
 console.dir(process.env);
 
-console.log(process.env['http_proxy']);
-console.log(process.env.http_proxy);
+console.log(process.env['HOME']);
+console.log(process.env.HOME);
 
 // EOF
