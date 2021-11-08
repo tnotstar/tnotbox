24c03859250a467a515694996d3f1b3107e995eb
Antonio Alvarado Hernández <tnotstar@gmail.com>
2021-11-08 23:55:07 +0100

    fixed minor issues from `pytest0` webinar

--- Python/Learn/Pytest0/test_port1.py
+++ Python/Learn/Pytest0/test_port1.py
@@ -9,6 +9,6 @@ class PortfolioTest(unittest.TestCase):
     def test_buy_one_stock(self):
         p = Portfolio()
         p.buy("IBM", 100, 176.48)
-        assert p.cost() == 17648
+        assert p.cost() == 17648.0
 
 # EOF
--- Python/Learn/Pytest0/test_port2.py
+++ Python/Learn/Pytest0/test_port2.py
@@ -13,7 +13,7 @@ class PortfolioTest(unittest.TestCase):
     def test_buy_one_stock(self):
         p = Portfolio()
         p.buy("IBM", 100, 176.48)
-        assert p.cost() == 17648
+        assert p.cost() == 17648.0
 
     def test_buy_two_stocks(self):
         p = Portfolio()
