0cab73dec5765ac0ed382e066caa7bc9856a6fc0
Antonio Alvarado Hernández <tnotstar@gmail.com>
2022-09-08 17:27:36 +0200

    updated `denoServer.ts` example

--- TypeScript/Deno/Examples/denoServer.ts
+++ TypeScript/Deno/Examples/denoServer.ts
@@ -1,16 +1,13 @@
-import {serve} from "https://deno.land/std/http/server.ts"
+import { serve } from "https://deno.land/std/http/server.ts"
 
 const hostname = '127.0.0.1';
 const port = 3001;
 
-const server = serve({hostname: hostname, port: port})
-console.log(`Server running at http://${hostname}:${port}/`)
-for await (const req of server) {
-    const headers = new Headers()
-    headers.set("Content-Type", "text/plain")
-    req.respond({
-        status: 200,
-        headers: headers,
-        body: "Hello, world!"
+console.log(`Server running at http://${hostname}:${port}/`);
+serve(() => {
+    return new Response("Hello, world!", {
+        headers: {
+            "Content-Type": "text/plain",
+        },
     })
-}
+}, { hostname, port });
