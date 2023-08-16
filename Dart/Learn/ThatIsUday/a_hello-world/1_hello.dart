b1b255b051320030538283fbd8a108dd5835af50
Antonio Alvarado Hernández <tnotstar@gmail.com>
2023-08-16 12:26:33 +0200

    removed legacy `dart` sample code

--- Dart/Learn/ThatIsUday/a_hello-world/1_hello.dart
+++
@@ -1,9 +0,0 @@
-// Stolen from: https://itnext.io/e17c35a60c54
-
-/**
- * `main` function is the entry point of execution when a .dart file
- * is executed using `dart` command (the dart vm)
- */
-void main() {
-    print("Hello, world!"); // print a string to the standard output
-}
--- Dart/Learn/ThatIsUday/a_hello-world/2_hello.dart
+++
@@ -1,9 +0,0 @@
-/**
- * `main` function could have arguments!!
- */
-void main(List<String> args) {
-    if (args.length > 0)
-        print("Hello, " + args[0] + "!");
-    else
-        print("Hello, world!");
-}
