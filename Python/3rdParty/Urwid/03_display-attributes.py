9cc17045ade9b68d52997c463d4421b5dd9f3669
Antonio Alvarado Hernández <tnotstar@gmail.com>
2023-09-13 10:22:16 +0200

    fixed some identifiers in `display-attributes` example

--- Python/3rdParty/Urwid/03_display-attributes.py
+++ Python/3rdParty/Urwid/03_display-attributes.py
@@ -6,20 +6,20 @@ import urwid as uw
 PALETTE = [
     ("banner", "black", "yellow"),
     ("streak", "black", "dark red"),
-    ("bg",     "black", "dark blue"),
+    ("bkgrnd", "black", "dark blue"),
 ]
 
 def main():
     text = uw.Text(("banner", "Hello, World! (Press Q to exit)"), align="center")
     filler = uw.Filler(uw.AttrMap(text, "streak"))
-    mapper = uw.AttrMap(filler, "bg")
+    mapper = uw.AttrMap(filler, "bkgrnd")
 
     def show_or_exit(key):
         if key in ('q', 'Q', 'esc'):
             raise uw.ExitMainLoop()
         text.set_text(str(key))
 
-    loop = uw.MainLoop(mapper, palette=PALETTE, unhandled_input=show_or_exit)  # type: ignore
+    loop = uw.MainLoop(mapper, PALETTE, unhandled_input=show_or_exit)  # type: ignore
     loop.run()
 
 
